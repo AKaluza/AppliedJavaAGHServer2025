09d737140e195a7307fd356f61837317bc08000a
andrzej <andrzejkaluza92@gmail.com>
2025-12-17 19:30:19 +0100

    READMEeng.md

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -12,7 +12,7 @@ import java.nio.file.Path;
 public class Main {
     public static void main(String[] args) throws IOException {
 //        final var cave = new SimpleCave(60, 160);
-        final var cave = new DrunkenCave(60, 160);
+        final var cave = new DrunkenCave(15, 50);
         final var game = new Game(cave);
         //final var server = new Server(game, "{\"known\":[{\"authorize\":{\"type\":\"A\",\"key\":\"1234\"},\"player\":{\"type\":\"P\",\"name\":\"Player0\"}}]}");
         final var server = new Server(game, Path.of("config/configuration.json"));
